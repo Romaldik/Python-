2fe4b6009da88208f9be0bb9e1534540ba2c9d71
Romaldik <romasepel955@gmail.com>
2024-11-01 21:10:43 +0200

    TeamMember abstractclass

--- src/TeamMember.py
+++ src/TeamMember.py
@@ -1,1 +1,19 @@
+from abc import ABC, abstractmethod
 
+class TeamMember(ABC):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    @abstractmethod  
+    def train(self):
+        pass
+    
+    @abstractmethod
+    def analyze_perfomance(self):
+        pass
+    
+    def __str__(self):
+        return f"{self.name}, {self.age} років"
+    
+    
