f99f0e3059c072fa12d228ad4085039b51e422df
Terlok <alexzavalij5@gmail.com>
2024-11-09 22:25:04 +0200

    Added methods to Classes Player, Coach, Staff. Added function select_query, execute_query to db_utils

--- src/DataBase/db_utils.py
+++ src/DataBase/db_utils.py
@@ -1,20 +1,40 @@
 from . import create_connection
 
-class dbUtils:
-    def __init__(self) -> None:
-        self.connection = create_connection()
+def get_table_list():
+    connection = create_connection()
+    if connection:
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+                tables = cursor.fetchall()
+                table_names = [table[0] for table in tables]
+                return table_names
+        except Exception as e:
+            print(f"Error fetching table list: {e}")
+        finally:
+            connection.close()
+    return []
 
-    def get_table_list(self,):
-        if self.connection:
-            try:
-                with self.connection.cursor() as cursor:
-                    cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
-                    tables = cursor.fetchall()
-                    table_names = [table[0] for table in tables]
-                    return table_names
-            except Exception as e:
-                print(f"Error fetching table list: {e}")
-            finally:
-                self.connection.close()
-        return []
-    
+def select_query(query, value):    
+    connection = create_connection()
+    if connection:
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute(query, value)
+                return cursor.fetchone()
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            connection.close()
+    return None
+
+def execute_query(query, value):    
+    connection = create_connection()
+    if connection:
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute(query, value)
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            connection.close()
--- src/mypackage/TeamMember.py
+++ src/mypackage/TeamMember.py
@@ -1,60 +1,92 @@
 from abc import ABC, abstractmethod
+from ..DataBase.db_utils import select_query, execute_query 
 
 class TeamMember(ABC):
-    def __init__(self, name, age):
+    def __init__(self, name:str, age:int, team:str):
         self.name = name
         self.age = age
+        self.team = team
     
     @abstractmethod  
-    def train(self):
+    def add_people(self):
         pass
-    
-    @abstractmethod
-    def analyze_perfomance(self):
+
+    @abstractmethod  
+    def delete_people(self):
         pass
-    
+        
     def __str__(self):
-        return f"{self.name}, {self.age} років"
+        pass
     
-# Підклас абстрактного класу TeamMember 
 class Player(TeamMember):
-    def __init__(self, name, age, skills, role):
-        super().__init__(name, age)
-        self.skills = skills
+    def __init__(self, name, nickname, age, role, team):
+        super().__init__(name, age, team)
+        self.nickname = nickname
         self.role = role
-        
-    def train(self):
-        print(f"{self.name} тренується для покращення навичок.")
-        
-    def analyze_perfomence(self):
-        print(f"Аналіз виступу гравця {self.name}. Його навички: {self.skills}")
-        
+    
+    def add_people(self):
+        query = 'SELECT id FROM Team WHERE name = %s;'
+        team_id = select_query(query, (self.team,))
+
+        add_player = """
+        INSERT INTO Player (name, nickname, age, role, team_id)
+        VALUES (%s, %s, %s, %s, %s);
+        """
+        data = (self.name, self.nickname, self.age, self.role, team_id)
+        execute_query(add_player, data)
+
+    def delete_people(self):
+        query = 'SELECT id FROM Player WHERE name = %s;'
+        player_id = select_query(query, (self.name,))
+
+        delete_query = "DELETE FROM Player WHERE id = %s"
+        execute_query(delete_query, player_id)
+    
     def __str__(self):
         return f"{self.name} ({self.role}), навички {self.skills}"
     
-# Підклас абстрактного класу TeamMember
 class Coach(TeamMember):
-    def __init__(self, name, age, style):
-        super.__init__(name, age)
-        self.style = style
+    def __init__(self, name, nickname, age, team):
+        super.__init__(name, age, team)
+        self.nickname = nickname
         
-    def train(self):
-        print(f"Тренер {self.name} проводить тренування в стилі{self.style}")
-        
-    def analyze_perfomance(self):
-        print(f"Тренер {self.name} аналізує результати команди.")
+    def add_people(self):
+        query = 'SELECT id FROM Team WHERE name = %s;'
+        team_id = select_query(query, (self.team,))
 
-# Підклас абстрактного класу TeamMember
+        add_coach = """
+        INSERT INTO Coach (name, nickname, age, team_id)
+        VALUES (%s, %s, %s, %s);
+        """
+        data = (self.name, self.nickname, self.age, team_id)
+        execute_query(add_coach, data)
+
+    def delete_people(self):
+        query = 'SELECT id FROM Coach WHERE name = %s;'
+        coach_id = select_query(query, (self.name,))
+
+        delete_query = "DELETE FROM Coach WHERE id = %s"
+        execute_query(delete_query, coach_id)
+
 class Staff(TeamMember):
-    def __init__(self, name, age, position):
-        super().__init__(name, age)
-        self.position = position
+    def __init__(self, name, age, role, team):
+        super().__init__(name, age, team)
+        self.role = role
     
-    def train(self):
-        print(f"{self.position} {self.name} надає підтримку команді.")
+    def add_people(self):
+        query = 'SELECT id FROM Team WHERE name = %s;'
+        team_id = select_query(query, (self.team,))
 
-    def analyze_perfomance(self):
-        print(f"{self.position} {self.name} проводить аналіз виступів команди.")
+        add_staff = """
+        INSERT INTO Staff (name, age, role, team_id)
+        VALUES (%s, %s, %s, %s);
+        """
+        data = (self.name, self.age, self.role, team_id)
+        execute_query(add_staff, data)
 
-    def __str__(self):
-        return f"{self.position} {self.name}, {self.age} років"
+    def delete_people(self):
+        query = 'SELECT id FROM Staff WHERE name = %s;'
+        staff_id = select_query(query, (self.name,))
+
+        delete_query = "DELETE FROM Staff WHERE id = %s"
+        execute_query(delete_query, staff_id)
